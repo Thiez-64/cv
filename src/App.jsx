fa787e9e1c3bbe6337c2187cbc7ddc677ea41b99
Thiez-64 <ma.ojeanson@gmail.com>
2021-04-02 19:25:47 +0200

    Determination of the zones

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,22 @@
 import React, { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
+import Hobbies from './components/Hobbies'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <div className="grid-container">
+      <div className="picture">Picture</div>
+      <div className="contact">Contact</div>
+      <div className="introduction">Introduction</div>
+      <div className="softskills">Soft-Skills</div>
+      <div className="education">Education</div>
+      <div className="experience">Experiences</div>
+      <div className="hardskills">Hard-Skills</div>
+        <Hobbies />
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Hobbies.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './hobbies.css';
+
+function Hobbies() {
+    return (
+      <div className="hobbies">
+        <h2>Hobbies</h2>
+        <div className="hobbies-box">
+          <div className="sport">Sport</div>
+          <div className="environment">Environment</div>
+          <div className="streetart">Street-Art</div>
+          <div className="mechanical">Mechanical</div>
+        </div>
+      </div>
+    )
+  }
+  
+export default Hobbies;
